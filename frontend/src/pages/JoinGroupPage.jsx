43b7f2e60205f62ddd0e3207e6c02570c1fe270f
mliu947 <mingmingliu3517@gmail.com>
2025-04-20 20:10:25 +1200

    create page and css files

---
+++ frontend/src/pages/JoinGroupPage.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styles from "../styles/JoinGroupPage.module.css";
+import { AuthContext } from "../contexts/AuthContext";
+import MobileFrame from "../components/MobileFrame";
+
+export default function JoinGroupPage() {
+    <MobileFrame>
+
+    </MobileFrame>
+}
