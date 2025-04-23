3717c80aaec47c8108667896abd1e3e2f1004060
Pan <pwan744@aucklanduni.ac.nz>
2025-04-23 18:12:11 +1200

    expense bill list

--- backend/src/controllers/billController.js
+++ backend/src/controllers/billController.js
@@ -18,6 +18,8 @@ export const getBillsByGroupId = async (req, res) => {
   try {
     const { groupId } = req.params;
 
+    console.log("🔍 Matching groupId:", groupId, "→", new mongoose.Types.ObjectId(groupId));
+
     const bills = await Bill.aggregate([
         { $match: { groupId: new mongoose.Types.ObjectId(groupId) } },
         { $unwind: "$groupBills" },  // 拆开 groupBills
@@ -26,13 +28,23 @@ export const getBillsByGroupId = async (req, res) => {
             from: "labels",
             localField: "groupBills.labelId",
             foreignField: "_id",
-            as: "groupBills.label"
+            as: "label"
           }
         },
-        { $unwind: "$groupBills.label" }, // 把 label 也展开
+        { $unwind: { path: "$label", preserveNullAndEmptyArrays: true } },
         {
-          $replaceRoot: { newRoot: "$groupBills" }
-        }
+            $project: {
+              _id: "$groupBills._id", // 可以换成 "$_id" 保留原 Bill 文档的 id
+              label: "$label",
+              date: "$groupBills.date",
+              note: "$groupBills.note",
+              paidBy: "$groupBills.paidBy",
+              expenses: "$groupBills.expenses",
+              refunds: "$groupBills.refunds",
+              splitWay: "$groupBills.splitWay",
+              members: "$groupBills.members",
+            }
+        }          
     ]);
 
     res.status(200).json(bills);
